23cc9892407b12ffd39728f559e6fe928c8a269e
tisharps <tisharps@gmail.com>
2016-01-31 16:15:50 -0200

    Splash Background e tela de creditos

---
+++ Game/Assets/Scripts/Platforms/Menus/creditScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditScreen : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetButtonDown("Fire1"))
+        {
+            if (Input.anyKeyDown)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
