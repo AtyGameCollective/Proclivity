e65fb5caf88b223d05c3378df19c3d57a2f3cfc6
joaogameprog <joaogameprog@gmail.com>
2016-01-31 02:35:49 -0200

    Items Coletáveis

--- Game/Assets/Scripts/Platforms/CollectableItem.cs
+++ Game/Assets/Scripts/Platforms/CollectableItem.cs
@@ -1,15 +1,80 @@
 ﻿using UnityEngine;
+
 using System.Collections;
 
 public class CollectableItem : MonoBehaviour {
+	Collider2D coll2D;
+	bool active;
+	SpriteRenderer sRenderer;
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	int itemOrder = 0;
+
+	public int ItemOrder {
+		get {
+			return itemOrder;
+		}
+		set {
+			itemOrder = value;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	int playerItemIndex = -1;
+
+	void OnEnable()
+	{
+		sRenderer = GetComponent<SpriteRenderer> ();
+		coll2D = GetComponent<Collider2D> ();
+		active = true;
 	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player") {
+			playerItemIndex = col.GetComponent<PlayerInventory>().ItemsCollectedCount;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.tag == "Player") {
+			if (Input.GetButtonDown ("Fire1")) {
+				if (active && playerItemIndex == itemOrder) {
+					active = false;
+				} else {
+					return;
+				}
+				coll2D.enabled = false;
+				col.GetComponent<PlayerInventory> ().ItemCollected (this);
+				StartCoroutine (CollectItemAnimation ());
+			}
+		}
+
+	}
+
+	IEnumerator CollectItemAnimation()
+	{
+		yield return null;
+		Debug.Log (gameObject.name + " Collected");
+		float time = 0;
+		float totalTime = 1f;
+		Vector3 startScale = transform.localScale;
+		Vector3 endScale = startScale * 3f;
+		Color startColor = sRenderer.color;
+		Color endColor = Color.clear;
+
+
+		while (time < totalTime) {
+			time += Time.deltaTime;
+			yield return null;
+			sRenderer.color = Color.Lerp(startColor, endColor,time/totalTime);
+			transform.localScale = Vector3.Lerp (startScale, endScale, time / totalTime);
+
+		}
+
+		sRenderer.color =  endColor;
+		transform.localScale = endScale;
+		gameObject.SetActive (false);
+	}
+
 }
