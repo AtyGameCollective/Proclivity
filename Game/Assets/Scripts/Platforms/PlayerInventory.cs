3c9131145b3bdf4af93fefc2cf63a1d8b1fad481
joaogameprog <joaogameprog@gmail.com>
2016-01-31 15:09:00 -0200

    Minigame Activator

--- Game/Assets/Scripts/Platforms/PlayerInventory.cs
+++ Game/Assets/Scripts/Platforms/PlayerInventory.cs
@@ -4,10 +4,25 @@ using System.Collections.Generic;
 
 public class PlayerInventory : MonoBehaviour {
 	List<int> itemsCollected= new List<int>();
+	int itemsNeeded;
+	[SerializeField]
+	Collider2D doorCollider;
+	void Awake()
+	{
+		itemsNeeded = ApplicationModel.CurrentLevel+2;
+	}
 
 	public int ItemsCollectedCount{ get { return itemsCollected.Count; } }
 	public void ItemCollected(CollectableItem item)
 	{
 		itemsCollected.Add(item.ItemOrder);
+		OpenDoor ();
+	}
+
+	void OpenDoor()
+	{
+		if (ItemsCollectedCount == itemsNeeded) {
+			doorCollider.enabled = true;
+		}
 	}
 }
